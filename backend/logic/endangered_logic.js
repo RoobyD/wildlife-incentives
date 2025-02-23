3e35d6d10366f3063a25027b65349fa40e7dd088
Miles Brandl <brandlm1@lions.tcnj.edu>
2025-02-23 03:03:16 -0500

    more logic

--- backend/logic/endangered_logic.js
+++ backend/logic/endangered_logic.js
@@ -1,7 +1,28 @@
-function recommend_protection(county, acreage) {
+function recommendProtection(county, acreage) {
 
+protection_profit = 0;
+let protected_species = [];
 
+    if (county === "Valley" || county === "Hill" || county === "Blaine" || county === "Phillips"){
+    protection_profit = acreage * 0.75
+    protected_species = ["Swift fox"];
+    }
+    else if (county === "Valley" || county === "Hill" || county === "Blaine" || county === "Phillips"):
+        protection_profit = acreage * 0.75
+        protected_species = ["Swift fox"];
+        
+    if (county === "Valley" || county === "Hill" || county === "Blaine" || county === "Phillips")
+    protection_profit = acreage * 0.75
+    protected_species = ["Swift fox"];
 
-
-
+    if (county === "Valley" || county === "Hill" || county === "Blaine" || county === "Phillips")
+        protection_profit = acreage * 0.75
+        protected_species = ["Swift fox"];
+        
 }
+    return {
+        protection_profit,
+        protected_species
+    };
+
+export default recommendProtection;
